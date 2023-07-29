ddc52ca2168cd90c676ba606174899b3ec7902dc
OmegaEpidex <84409902+OmegaEpidex@users.noreply.github.com>
2023-07-29 05:35:09 +0000

    Late alterations
    Tired

--- NV/frontend/src/components/Header.js
+++ NV/frontend/src/components/Header.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="site-header">
-      <h1>Welcome to Your Social Media Dashboard</h1>
-      {/* Add navigation links or other header content here */}
+      <h1>Welcome to the Media Sphere</h1>
+      {
+        <div class="auth-button">
+          <button class="auth-button">
+            Sign in / Sign up
+        </button>
+        </div>
+      }
     </header>
   );
 };
